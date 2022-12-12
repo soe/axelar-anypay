8b017b9bc9c0dd46dc32fbc6c77b5959c067292c
soe <soe@soe.im>
2022-12-13 04:19:33 +0630

    image vs img tag

--- pages/[pid].tsx
+++ pages/[pid].tsx
@@ -2,7 +2,6 @@ import { JsonRpcSigner } from '@ethersproject/providers';
 import cn from 'classnames';
 import { Wallet } from 'ethers';
 import type { NextPage } from 'next';
-import Image from 'next/image'
 import { useRouter } from 'next/router'
 import React, { useCallback, useEffect, useState } from 'react';
 import { getSenderWallet, isTestnet } from '../config/constants';
@@ -118,7 +117,7 @@ const Home: NextPage = () => {
                 {/* source chain card */}
                 <div className="row-span-2 shadow-xl card w-96 bg-base-100">
                     <div className="text-center p-6 pb-0">
-                        <Image
+                        <img
                             src={payee.avatar}
                             className="rounded-full w-24 mb-4 mx-auto"
                             alt="Avatar"
